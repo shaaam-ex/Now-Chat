33ecef2191bae2ddb5ebe1d1f79367cf4bb6d548
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-05 20:14:05 +0500

    v0.01 Landing Page customization started.

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -2,7 +2,23 @@ import './Landing.css';
 
 const Landing = () => {
     return (
-        <div>Landing Page</div>
+        <>
+            <div className="main-container-landing-page">
+                <div className="first-section-landing-page">
+                    <div className="first-inner-first-section-landing-page">
+                        <h1>Chat anywhere, Anytime <br /> Seamlessly</h1>
+                        <img src="https://res.cloudinary.com/dm1hjjfsz/image/upload/v1720108216/Now/Now-logo/vector/default-monochrome_vrthcc.svg" alt="Now Logo Main" />
+                    </div>
+                </div>
+
+                <div className="second-section-landing-page">
+                    <div className="first-inner-second-section-landing-page">
+                    <img src="https://img.freepik.com/free-photo/medium-shot-smiley-family-with-tablet_23-2148936768.jpg?size=626&ext=jpg&ga=GA1.1.523418798.1710374400&semt=ais" alt="Happy Family" />
+                        <h1>Chat with your friends, family, and colleagues <br /> with ease</h1>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
