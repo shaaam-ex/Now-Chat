116da4abcd8cc361ba3981a45d2e4bff24dadf3e
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-06 20:54:19 +0500

    v0.01 Landing Page customization started.

--- frontend/src/components/Layouts/Header/Header.jsx
+++ frontend/src/components/Layouts/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <header className="main-container">
-                <img src="https://res.cloudinary.com/dm1hjjfsz/image/upload/v1720108237/Now/N-logo/vector/default-monochrome_bgdm61.svg" alt="N Logo Header" />
+                <img src="https://res.cloudinary.com/dm1hjjfsz/image/upload/v1720108237/Now/N-logo/vector/default-monochrome_bgdm61.svg" alt="N Logo Header" draggable="false" />
                 <nav>
                     <ul>
                         <li><a href='#'>Home</a></li>
