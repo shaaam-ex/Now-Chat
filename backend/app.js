6372dc56d70ee9b467c8c1bca8cc8007cadae1f6
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-07 17:39:23 +0500

    v0.02 Work started on backend

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const errorMiddleware = require('./middleware/error');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'backend/config/config.env' });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Import all routes
+const user = require('./routes/user');
+
+app.use('/api', user);
+
+// Middleware to handle errors
+app.use(errorMiddleware);
+
+module.exports = app;
