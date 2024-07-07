3fc77f58b07b8916581946d5df2a6c6278a2c68e
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-07 17:11:09 +0500

    v0.02 Work started on backend

---
+++ backend/models/User/userModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength: [30, 'Your name cannot exceed 30 characters'],
+        minLength: [4, 'Your name must be at least 4 characters long'],
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter a valid email address'],
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6, 'Your password must be at least 6 characters long'],
+        select: false,
+    },
+
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+// JWT Token
+UserSchema.methods.getJwtToken = function () {
+    // Generating token
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+}
