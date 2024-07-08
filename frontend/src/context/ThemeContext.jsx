82bcd32ace7115cc429ac88fcf9f2d4f71c022aa
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-08 11:52:49 +0500

    v0.02 Added Light/Dark Mode

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext(undefined);
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("Dark");
+    const [theme, setTheme] = useState("Light");
 
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
