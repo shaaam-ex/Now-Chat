001dec95103a45ba80c5e5e8e9fda4a4122c98da
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-10 22:05:47 +0500

    v0.03 Started work on Redux

--- frontend/src/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import Cookies from 'js-cookie';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = () => {
 
     const { isAuthenticated } = useSelector(state => state.user);
 
-    return !isAuthenticated ? <Outlet /> : <Navigate to="/" />; // outlet for nested routes
+    return !isAuthenticated ? <Outlet /> : <Navigate to="/chat" />; // outlet for nested routes
 };
 
 export default ProtectedRoute;
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -7,7 +7,10 @@ import {
     REGISTER_FAIL,
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL
 } from '../constants/userConstant';
 
 
@@ -15,24 +18,20 @@ export const userReducer = (state = { user: {} }, action) => {
     switch(action.type) {
         case LOGIN_REQUEST:
         case REGISTER_REQUEST:
+        case LOAD_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false
             }
 
         case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+        case LOAD_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isAuthenticated: true,
                 user: action.payload
-            }
-
-        case REGISTER_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                isAuthenticated: false,
             }
 
         case LOGOUT_SUCCESS:
@@ -44,6 +43,7 @@ export const userReducer = (state = { user: {} }, action) => {
         
         case LOGIN_FAIL:
         case REGISTER_FAIL:
+        case LOAD_USER_FAIL:
             return {
                 ...state,
                 loading: false,
