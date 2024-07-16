40714ba55ae313e1456f701016933436256c14d1
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-16 21:57:37 +0500

    v0.04 Added load state

--- frontend/src/components/ChatHome/ChatHome.jsx
+++ frontend/src/components/ChatHome/ChatHome.jsx
@@ -5,6 +5,10 @@ import './ChatHome.css';
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
+import { Link } from "react-router-dom";
+
+import { PiGearFill } from "react-icons/pi";
+
 
 const ChatHome = () => {
     
@@ -28,6 +32,14 @@ const ChatHome = () => {
         loading ? <Loading /> :
         <div className="main-container-chat-home-page">
             <div className="left-container-chat-home-page">
+                <div className="current-user-top-left-container">
+                    <img src={"https://scontent.fisb5-2.fna.fbcdn.net/v/t39.30808-1/406478511_2111345645882145_1908622622939664747_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=JGSfoC9OElUQ7kNvgEw__eQ&_nc_ht=scontent.fisb5-2.fna&oh=00_AYB-e-aaFGbeZ2Z22db9VQa4xqsc9ikUQ6eOP6OMsmym8g&oe=669C7202"} alt="User Profile Image" />
+                    
+                    <div className="inner-container-current-user-top-left">
+                        <h3>{user.name}</h3>
+                        <Link className="small-button-transparent" to={'/'}><PiGearFill /></Link>
+                    </div>
+                </div>
             </div>
 
             <div className="right-container-chat-home-page">
