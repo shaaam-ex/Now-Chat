f7a776d3566b37791be29ad0a4efc92aecbcaa0d
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-07-16 22:59:23 +0500

    v0.05 Work started on chat home page.

--- frontend/src/components/ChatHome/ChatHome.jsx
+++ frontend/src/components/ChatHome/ChatHome.jsx
@@ -3,11 +3,13 @@ import Loading from "../Layouts/Loading/Loading";
 
 import './ChatHome.css';
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 
 import { PiGearFill } from "react-icons/pi";
+import Meta from "../Layouts/Meta/Meta";
+import ConnectedUser from "../Layouts/chat/ConnectedUser/ConnectedUser";
 
 
 const ChatHome = () => {
@@ -15,6 +17,21 @@ const ChatHome = () => {
     const { isAuthenticated, loading, user } = useSelector(state => state.user);
 
     const navigate = useNavigate();
+
+    let [friends, setFriends] = useState([
+        {
+            name: 'Hassaan Naushad',
+            profileimgUrl: 'https://scontent.fisb5-1.fna.fbcdn.net/v/t39.30808-1/347397970_289099423531073_3789015277106402604_n.jpg?stp=dst-jpg_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=F-qBzmVPq28Q7kNvgEkCogb&_nc_ht=scontent.fisb5-1.fna&oh=00_AYCgAI6LG1mebS1pGtLATK_WFRDWDIRRANFlTQpz2oKfYQ&oe=669CA14A'
+        },
+        {
+            name: 'Zain Malik',
+            profileimgUrl: 'https://scontent.fisb5-2.fna.fbcdn.net/v/t39.30808-1/439159181_825177422810823_6050681095915403484_n.jpg?stp=dst-jpg_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=iHqznlI2eLYQ7kNvgEJ55mX&_nc_ht=scontent.fisb5-2.fna&oh=00_AYC1TcwVviGMPKJPfmoGU1I0-lXcUTtdAdhsN15KwOi2nw&oe=669C77AC'
+        },
+        {
+            name: 'Muhammad Ebad',
+            profileimgUrl: 'https://scontent.fisb5-2.fna.fbcdn.net/v/t39.30808-1/448680290_2022316958170712_1089718321175194788_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=R1MAu1Rd4aEQ7kNvgHJdrmN&_nc_ht=scontent.fisb5-2.fna&oh=00_AYDKSOIm7bbbk2vrOvlSpOsE5E-3NCss8-2xtFXbgQwM6w&oe=669C93E8'
+        },
+    ])
 
     useEffect(() => {
         if(!isAuthenticated) {
@@ -30,21 +47,39 @@ const ChatHome = () => {
 
     return (
         loading ? <Loading /> :
-        <div className="main-container-chat-home-page">
-            <div className="left-container-chat-home-page">
-                <div className="current-user-top-left-container">
-                    <img src={"https://scontent.fisb5-2.fna.fbcdn.net/v/t39.30808-1/406478511_2111345645882145_1908622622939664747_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=JGSfoC9OElUQ7kNvgEw__eQ&_nc_ht=scontent.fisb5-2.fna&oh=00_AYB-e-aaFGbeZ2Z22db9VQa4xqsc9ikUQ6eOP6OMsmym8g&oe=669C7202"} alt="User Profile Image" />
+        <>
+        <head>
+            <Meta title="Chat" />
+        </head>
+        
+        <body>
+            <main>
+                <div className="main-container-chat-home-page">
+                    <div className="left-container-chat-home-page">
+                        <div className="current-user-top-left-container">
+                            <img src={"https://scontent.fisb5-2.fna.fbcdn.net/v/t39.30808-1/406478511_2111345645882145_1908622622939664747_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=JGSfoC9OElUQ7kNvgEw__eQ&_nc_ht=scontent.fisb5-2.fna&oh=00_AYB-e-aaFGbeZ2Z22db9VQa4xqsc9ikUQ6eOP6OMsmym8g&oe=669C7202"} alt="User Profile Image" />
                     
-                    <div className="inner-container-current-user-top-left">
-                        <h3>{user.name}</h3>
-                        <Link className="small-button-transparent" to={'/'}><PiGearFill /></Link>
+                            <div className="inner-container-current-user-top-left">
+                                <h3>{user.name}</h3>
+                                <Link className="small-button-transparent" to={'/'}><PiGearFill /></Link>
+                            </div>
+                        </div>
+
+                        <div className="friends-chat-container-left">
+                            {
+                                friends.map(curr => (
+                                    <ConnectedUser name={curr.name} profileimgUrl={curr.profileimgUrl} />
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className="right-container-chat-home-page">
                     </div>
                 </div>
-            </div>
-
-            <div className="right-container-chat-home-page">
-            </div>
-        </div>
+            </main>
+        </body>
+        </>
     )
 }
 
---
+++ frontend/src/components/Layouts/chat/ConnectedUser/ConnectedUser.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ConnectedUser = (props) => {
+    return (
+        <>
+            <div className="main-container-friend-left-profile">
+                <img src={props.profileimgUrl} alt="" />
+
+                <div className="inner-contaienr-friend-left-container">
+                    <h3>{props.name}</h3>
+                    <p>Recent Message</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ConnectedUser
